c8a00cb931cc728b3d1b795ce1fe7e89c2b8c42f
AmandaBirmingham <lists@imladris.com>
2024-06-24 17:55:24 -0700

    Added more sanity checking, error logging to manifest loading

--- nph_metadata/src/nph_metadata_generator.py
+++ nph_metadata/src/nph_metadata_generator.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
+from datetime import datetime
 import os
 import pandas
 from qiimp import  \
     HOSTTYPE_SHORTHAND_KEY, SAMPLETYPE_SHORTHAND_KEY, SAMPLE_TYPE_KEY, \
     extract_config_dict, load_df_with_best_fit_encoding, \
-    generate_extended_metadata_file_from_raw_metadata_df
+    write_extended_metadata_from_df
 from nph_metadata.src.nph_literals import *
 from nph_metadata.src.nph_transformers import format_real_vs_blanks_dates
 
@@ -33,7 +35,16 @@ def make_nph_extendable_metadata_df(
 
     # keep loading manifests in reverse chronological order until all samples
     # in the samples_df have been matched to a manifest
-    merged_df = _lazy_load_manifests(samples_df, manifests_dir)
+    merged_df, load_msgs_df = _lazy_load_manifests(samples_df, manifests_dir)
+    if len(load_msgs_df) > 0:
+        # write the load messages to a file
+        timestamp_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        load_msgs_fp = os.path.join(
+            os.path.dirname(core_file_fp),
+            f"{timestamp_str}_manifest_load_messages.csv")
+        load_msgs_df.to_csv(load_msgs_fp, index=False)
+        raise ValueError(f"Errors occurred during manifest loading; "
+                         f"see {load_msgs_fp}")
 
     extendable_metadata_df = _standardize_nph_input_metadata_df(
         merged_df, extraction_yyyy_mm)
@@ -46,6 +57,9 @@ def _lazy_load_manifests(samples_df, manifests_dir):
     missing_samples = samples_df[CORE_SAMPLE_NAME_KEY].tolist()
     manifests_found = False
 
+    load_msgs_cols = ["load_message", MANIFEST_FILE_NAME_KEY,
+                      MANIFEST_SAMPLE_ID_KEY]
+    load_msgs_df = pandas.DataFrame(columns=load_msgs_cols)
     for curr_manifest_fp in _get_fps_youngest_to_oldest(manifests_dir):
         manifests_found = True
 
@@ -55,10 +69,20 @@ def _lazy_load_manifests(samples_df, manifests_dir):
         if curr_manifest_fn.startswith("."):
             continue
 
-        curr_manifest_df = load_df_with_best_fit_encoding(curr_manifest_fp, "\t")
+        manifest_load_types = {MANIFEST_SAMPLE_ID_KEY: "string"}
+        curr_manifest_df = load_df_with_best_fit_encoding(
+            curr_manifest_fp, "\t", dtype=manifest_load_types)
         curr_manifest_df[MANIFEST_FILE_NAME_KEY] = curr_manifest_fn
-        curr_manifest_df[MANIFEST_SAMPLE_ID_KEY] = \
-            curr_manifest_df[MANIFEST_SAMPLE_ID_KEY].astype("string")
+
+        # drop curr_manifest_df records that have nan MANIFEST_SAMPLE_ID_KEY
+        curr_manifest_df.dropna(subset=[MANIFEST_SAMPLE_ID_KEY], inplace=True)
+        if curr_manifest_df.empty:
+            err_msg = (f"Manifest has no records with a non-null "
+                       f"'{MANIFEST_SAMPLE_ID_KEY}'")
+            load_msgs_df = _append_load_msg_to_df(
+                err_msg, curr_manifest_fn, "", load_msgs_df)
+            continue
+
         if aggregated_manifests_df is None:
             aggregated_manifests_df = curr_manifest_df
         else:
@@ -81,21 +105,95 @@ def _lazy_load_manifests(samples_df, manifests_dir):
             (~merged_df[CORE_SAMPLE_ID_KEY].str.contains(BLANK_SUBSTRING))
         # get the sample_names for the rows in the missing_samples_mask
         missing_samples = \
-            merged_df.loc[missing_samples_mask, CORE_SAMPLE_NAME_KEY].tolist()
+            merged_df.loc[missing_samples_mask, CORE_SAMPLE_ID_KEY].tolist()
         if len(missing_samples) == 0:
             break
     # end for each manifest
 
     if not manifests_found:
-        raise ValueError(
-            f"No manifests found in the directory '{manifests_dir}'")
-    if len(missing_samples) > 0:
-        raise ValueError(
-            f"These samples were not found in any manifest: {missing_samples}")
+        load_msgs_df = _append_load_msg_to_df(
+            f"No manifests found in the directory '{manifests_dir}'", "", "",
+            load_msgs_df)
+    else:
+        # remove the MERGE_KEY column from the merged_df before returning
+        merged_df.drop(columns=[MERGE_KEY], inplace=True)
 
-    # remove the MERGE_KEY column from the merged_df before returning
-    merged_df.drop(columns=[MERGE_KEY], inplace=True)
-    return merged_df
+        if len(missing_samples) > 0:
+            sorted_missing_samples = sorted(missing_samples)
+            for curr_missing_sample in sorted_missing_samples:
+                load_msgs_df = _append_load_msg_to_df(
+                    "Sample not found in any manifest", "", curr_missing_sample,
+                    load_msgs_df)
+
+        load_msgs_df = _sanity_check_aggregated_manifests(
+            aggregated_manifests_df, load_msgs_df)
+    # end if manifests weren't/were found
+
+    if len(load_msgs_df) > 0:
+        merged_df = None
+
+    return merged_df, load_msgs_df
+
+
+def _append_load_msg_to_df(load_msg, manifest_file, sample_id, load_msgs_df):
+    load_msg_dict = {"load_message": load_msg,
+                     MANIFEST_FILE_NAME_KEY: manifest_file,
+                     MANIFEST_SAMPLE_ID_KEY: sample_id}
+    add_df = pandas.DataFrame([load_msg_dict])
+    output_df = pandas.concat([load_msgs_df, add_df], ignore_index=True)
+    return output_df
+
+
+def _append_load_msgs_to_df(load_msg, input_df, load_msgs_df):
+    err_msg_df = input_df.loc[:,
+                 [MANIFEST_FILE_NAME_KEY, MANIFEST_SAMPLE_ID_KEY]]
+    err_msg_df["load_message"] = load_msg
+    err_msg_df = err_msg_df[load_msgs_df.columns]
+    load_msgs_df = pandas.concat([load_msgs_df, err_msg_df])
+    return load_msgs_df
+
+
+def _sanity_check_aggregated_manifests(aggregated_manifests_df, load_msgs_df):
+    sorted_agg_manifests_df = aggregated_manifests_df.sort_values(
+        by=[MANIFEST_SAMPLE_ID_KEY, MANIFEST_FILE_NAME_KEY])
+
+    # the manifest column is definitely different between records from
+    # different manifests, so pop it out of the aggregated_manifests_df
+    # so we can check for duplicates across manifests
+    sorted_agg_manifests_wo_manifest_col_df = \
+        sorted_agg_manifests_df[
+            sorted_agg_manifests_df.columns.drop(
+                MANIFEST_FILE_NAME_KEY)].copy()
+
+    # all rows that share sample ids with other row(s);
+    # keep = false means all duplicates are id'd, including first one
+    duplicate_sample_ids_mask = \
+        sorted_agg_manifests_wo_manifest_col_df.duplicated(
+            MANIFEST_SAMPLE_ID_KEY, keep=False)
+    # all rows that are 100% the same as another row
+    duplicates_mask = \
+        sorted_agg_manifests_wo_manifest_col_df.duplicated(keep=False)
+
+    if duplicates_mask.any():
+        # order the df by the sample id and then by the manifest file name;
+        # make sure to use df *with* manifest col so we can see which
+        # manifest each duplicate came from
+        duplicates_df = sorted_agg_manifests_df.loc[duplicates_mask]
+        load_msgs_df = _append_load_msgs_to_df(
+            "Duplicated record", duplicates_df, load_msgs_df)
+
+    # combine duplicate_sample_ids_mask and duplicates_mask to get any
+    # records that are not duplicates but share a sample id
+    duplicate_sample_ids_only_mask = duplicate_sample_ids_mask & \
+                                     ~duplicates_mask
+    if duplicate_sample_ids_only_mask.any():
+        duplicate_sample_ids_df = \
+            sorted_agg_manifests_df.loc[duplicate_sample_ids_only_mask]
+        load_msgs_df = _append_load_msgs_to_df(
+            "Non-duplicate record with same sample id",
+            duplicate_sample_ids_df, load_msgs_df)
+
+    return load_msgs_df
 
 
 def _get_fps_youngest_to_oldest(inputs_dir):
@@ -142,11 +240,11 @@ if __name__ == "__main__":
     # raw_metadata_fp = "/Users/abirmingham/Desktop/metadata/test_raw_metadata_short.xlsx"
     # raw_metadata_fp = "/Users/abirmingham/Desktop/metadata/test_nph_metadata_short.xlsx"
     # core_file_fp = "/Users/abirmingham/Desktop/metadata/mod_NPH_011 Sample Processing spreadsheet_SAS KL.csv"
-    core_file_fp = "/Users/abirmingham/Desktop/metadata/nph/NPH_016 Sample Processing spreadsheet_SAS KL.csv"
-    manifests_dir = "/Users/abirmingham/Desktop/metadata/nph/Manifests 3"
-    extraction_yyyy_mm = "2024-05"
+    core_file_fp = "/Users/abirmingham/Desktop/metadata/nph/NPH_017 Sample Processing spreadsheet_SAS KL.csv"
+    manifests_dir = "/Users/abirmingham/Desktop/metadata/nph/Manifests_06202024"
+    extraction_yyyy_mm = "2024-06"
     output_dir = "/Users/abirmingham/Desktop/"
-    output_base = "NPH_016"
+    output_base = "NPH_017"
 
     nph_config_dict = extract_config_dict(None, starting_fp=__file__)
     nph_extendable_metadata_df = make_nph_extendable_metadata_df(
@@ -155,6 +253,6 @@ if __name__ == "__main__":
     nph_transformers_dict = \
         {"format_real_vs_blanks_dates": format_real_vs_blanks_dates}
 
-    generate_extended_metadata_file_from_raw_metadata_df(
+    write_extended_metadata_from_df(
         nph_extendable_metadata_df, nph_config_dict, output_dir, output_base,
         study_specific_transformers_dict=nph_transformers_dict)
